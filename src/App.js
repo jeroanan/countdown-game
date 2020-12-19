9ac06542e46ba505a0bc995264be36b47acddd54
David Wilson <davidwil@posteo.de>
2020-12-19 10:04:36 +0000

    letter generation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+	super(props);
+
+	const maxConsonants = 5;
+	const maxVowels = 5;
+	const numberOfTiles = 9;
+	let letters = [];
+
+	for (let i=0; i<numberOfTiles; i++) {
+	    letters.push(this.getRandomLetter(letters, maxConsonants, maxVowels));
+	}
+	
+	this.state = {
+	    letters: letters,
+	    vowels: 0,
+	    consonants: 0,
+	    maxConsonants: 5,
+	    maxVowels: 5,
+	}
+    }
+
+    isVowel(l)
+    {
+	const vowels = ['A', 'E', 'I', 'O', 'U'];
+	return vowels.indexOf(l) > -1;
+    }
+
+    getRandomLetter(ls, maxConsonants, maxVowels) {
+
+	while (true) {
+	    const aCharCode = 65;
+	    const r = Math.floor((Math.random()*26));
+
+	    const l = String.fromCharCode(aCharCode + r);
+
+	    const vowelsSoFar = ls.filter((x) => this.isVowel(x)).length;
+	    const consonantsSoFar = ls.filter((x) => !this.isVowel(x)).length;
+
+	    if ((!this.isVowel(l)) && (consonantsSoFar < maxConsonants)) {
+		return l;
+	    } else if (this.isVowel(l) && (vowelsSoFar < maxVowels)) {
+		return l;
+	    }
+	}
+    }
+
+    render() {
+	const letters = []
+	let i = 1;
+	for (let l of this.state.letters) {
+	    i++;
+	    letters.push(<Letter key={i} letter={l} />);
+	}
+
+	return(
+	    <>
+	    {letters}
+		</>
+	);
+    }
+
+}
+
+function Letter(props) {
+
+    const theLetter = props.letter;
+
+    return (<span>{theLetter}</span>);
 }
 
 export default App;
